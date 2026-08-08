f2366f04259a115df51325c0023f0de750f8a743
Khanra321 <akashkhanra3063@gmail.com>
2026-08-08 21:57:08 +0530

    Veriable+input+output

---
+++ 0ProgrammingBasics.py
@@ -0,0 +1,25 @@
+# # What is a veriable?
+# # Veriable is a container that contain data(int, float, char, Bool)
+
+# age =23
+# name ="Akash"
+# Hight =5.4
+# print(" name: ",name,"\n","age: ",age,"\n","hight: ",Hight)
+
+
+# # Input(alwaya taken as string thats why need type custing) and Output(we use print())
+# n = input("Enter a value: ")
+# print(n)
+# print(type(n))
+
+#     # for type custing we use "int()", "chr()", "folat()".
+# p = int(input("Enter a number: "))
+# print(p, type(p))
+
+    # Use Map()
+# x,y = map(int,input("EnterTwoValueBySpace:").split())
+# print(f"{x} + {y} = {x+y}")
+
+
+
+
