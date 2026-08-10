e57ff4b3cea242c046c311e9268a67d957ad27ce
Khanra321 <akashkhanra3063@gmail.com>
2026-08-10 20:54:47 +0530

    if-else

--- 0ProgrammingBasics.py
+++ 0ProgrammingBasics.py
@@ -1,5 +1,4 @@
-# # What is a veriable?
-# # Veriable is a container that contain data(int, float, char, Bool)
+#   # 1. veriable: Veriable is a container that contain data(int, float, char, Bool)
 
 # age =23
 # name ="Akash"
@@ -7,18 +6,28 @@
 # print(" name: ",name,"\n","age: ",age,"\n","hight: ",Hight)
 
 
-# # Input(alwaya taken as string thats why need type custing) and Output(we use print())
+#   # 2. Input(alwaya taken as string thats why need type custing) and Output(we use print())
 # n = input("Enter a value: ")
 # print(n)
 # print(type(n))
 
-#     # for type custing we use "int()", "chr()", "folat()".
+    # for type custing we use "int()", "chr()", "folat()".
 # p = int(input("Enter a number: "))
 # print(p, type(p))
 
-    # Use Map()
+#   #Use Map()
 # x,y = map(int,input("EnterTwoValueBySpace:").split())
 # print(f"{x} + {y} = {x+y}")
+
+    # 3. if-else: if-else is used for decision making.
+
+
+age = int(input("Enter your age:"))
+
+if age < 18:
+    print("You are minor")
+else: 
+    print("you are adult.")
 
 
 
