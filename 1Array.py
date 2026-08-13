ba4f7d8bfd56bbec04516d629757cfbacd39914f
Khanra321 <akashkhanra3063@gmail.com>
2026-08-13 21:23:43 +0530

    Traverse by ind with O(n)

--- 1Array.py
+++ 1Array.py
@@ -1,4 +1,9 @@
-    # 1. Array traversal: means visiting every element of an array one by one.
-arr = [10,20,30,40,50]
-for a in arr:
-    print(a,end=",")
+#     # 1. Array traversal: means visiting every element of an array one by one.
+# arr = [10,20,30,40,50]
+# for a in arr:
+#     print(a,end=",")
+
+#     # Traversla by index  Time Complexcity: O(n), because array seen one time.
+# arr = [10,20,30,40,50]
+# for b in range(len(arr)):
+#     print(arr[b])
