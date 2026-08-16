338883568fc423dbdd8c93608c4c1e06afc23587
Khanra321 <akashkhanra3063@gmail.com>
2026-08-16 19:45:35 +0530

    sume of an array

--- 1Array.py
+++ 1Array.py
@@ -7,3 +7,18 @@
 # arr = [10,20,30,40,50]
 # for b in range(len(arr)):
 #     print(arr[b])
+
+#     # Confusing in append 
+# arr1 = [1,2,3]
+# arr2 = arr1
+# arr1.append(4)
+# print(arr2)
+#     # Sum of all element
+# arr = [1,2,3,4,5]
+# sum = 0
+
+# for i in arr:
+#     sum += i
+# print(sum)
+
+
