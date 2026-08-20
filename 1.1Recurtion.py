6147cb03db54378a6735acffd3b609a0f85ff8f6
Khanra321 <akashkhanra3063@gmail.com>
2026-08-20 21:41:16 +0530

    Recurtion call first then print

--- 1.1Recurtion.py
+++ 1.1Recurtion.py
@@ -1,11 +1,16 @@
-    # Recurtion: When a function call it self.
-def num(n):
-    if n == 0:      # Bass cass
-        return 0
-    print(n)        # First print  # Output: 5 4 3 2 1
-    num(n-1)        # Recurtion call
-num(5)              # Call function
+#     # Recurtion: When a function call it self.
+# def num(n):
+#     if n == 0:      # Bass cass
+#         return 0
+#     print(n)        # First print  # Output: 5 4 3 2 1
+#     num(n-1)        # Recurtion call
+# num(5)              # Call function
 
 
-    # Recurtion call first then print
-    
+#     # Recurtion call first then print
+# def num(n):
+#     if n == 0:
+#         return
+#     num(n-1)
+#     print(n)  # Output : 1 2 3 4 5
+# num(5)  
