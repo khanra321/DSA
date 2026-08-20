fc596046fa8cbafd303f9bc2db649c277eb82c99
Khanra321 <akashkhanra3063@gmail.com>
2026-08-20 21:52:19 +0530

    Factorial

--- 1.1Recurtion.py
+++ 1.1Recurtion.py
@@ -14,3 +14,12 @@
 #     num(n-1)
 #     print(n)  # Output : 1 2 3 4 5
 # num(5)  
+
+    # Factorial 
+def fact(n):
+    if n == 0:
+        return 1
+    count = n*fact(n-1)
+    return count
+c = int(input("Enter a number to find factorial: "))
+print(fact(c))
