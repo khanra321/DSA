5b4567263ecc903fd970f9d057fe24e49d351303
Khanra321 <akashkhanra3063@gmail.com>
2026-08-21 21:19:19 +0530

    Add 1 to N

--- 1.1Recurtion.py
+++ 1.1Recurtion.py
@@ -1,4 +1,4 @@
-#     # Recurtion: When a function call it self.
+#     # 1.Recurtion: When a function call it self.
 # def num(n):
 #     if n == 0:      # Bass cass
 #         return 0
@@ -7,7 +7,7 @@
 # num(5)              # Call function
 
 
-#     # Recurtion call first then print
+#     # 2. Recurtion call first then print
 # def num(n):
 #     if n == 0:
 #         return
@@ -15,11 +15,21 @@
 #     print(n)  # Output : 1 2 3 4 5
 # num(5)  
 
-    # Factorial 
-def fact(n):
-    if n == 0:
-        return 1
-    count = n*fact(n-1)
-    return count
-c = int(input("Enter a number to find factorial: "))
-print(fact(c))
+#     # 3.  Factorial 
+# def fact(n):
+#     if n == 0:
+#         return 1
+#     count = n*fact(n-1)
+#     return count
+# c = int(input("Enter a number to find factorial: "))
+# print(fact(c))
+
+#     # 4. sum of 1 to N
+# def sum(N):
+#     if N == 0:
+#         return 0
+#     count = N + sum(N-1)
+#     return count
+# p = int(input("Enter a number calculate 1 to N: "))
+# print(sum(p))
+
