caa1da5c26f48eb34a0b79f7a772b5256bba5808
Khanra321 <akashkhanra3063@gmail.com>
2026-09-05 20:58:05 +0530

    Selection short

--- 2.0 Shorting.py
+++ 2.0 Shorting.py
@@ -1,9 +1,23 @@
 """Bubble short"""
 
+# arr = [23,32,54,3,6,13,1]
+# l = len(arr)
+# for i in range(l-1):
+#     for j in range(l-i-1):
+#         if arr[j] > arr[j+1]:
+#             arr[j], arr[j+1] = arr[j+1], arr[j]
+# print(arr)
+
+"""Selection short"""
 arr = [23,32,54,3,6,13,1]
 l = len(arr)
 for i in range(l-1):
-    for j in range(l-i-1):
-        if arr[j] > arr[j+1]:
-            arr[j], arr[j+1] = arr[j+1], arr[j]
+    min = i
+    for j in range(i+1, l):
+        if arr[j] < arr[min]:
+            min = j
+        else: 
+            continue
+    arr[i], arr[min] = arr[min], arr[i]
 print(arr)
+        
