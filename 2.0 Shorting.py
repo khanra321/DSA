acadccd3f5aff75873bdac2a5edb9592f73faa08
Khanra321 <akashkhanra3063@gmail.com>
2026-09-05 21:52:54 +0530

    Navbar

--- 2.0 Shorting.py
+++ 2.0 Shorting.py
@@ -9,15 +9,15 @@
 # print(arr)
 
 """Selection short"""
-arr = [23,32,54,3,6,13,1]
-l = len(arr)
-for i in range(l-1):
-    min = i
-    for j in range(i+1, l):
-        if arr[j] < arr[min]:
-            min = j
-        else: 
-            continue
-    arr[i], arr[min] = arr[min], arr[i]
-print(arr)
+# arr = [23,32,54,3,6,13,1]
+# l = len(arr)
+# for i in range(l-1):
+#     min = i
+#     for j in range(i+1, l):
+#         if arr[j] < arr[min]:
+#             min = j
+#         else: 
+#             continue
+#     arr[i], arr[min] = arr[min], arr[i]
+# print(arr)
         
